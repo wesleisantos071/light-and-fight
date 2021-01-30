f19ca1977c2cdded9c86ea2d05b5225f60beb5ad
Weslei <wesleisantos071@gmail.com>
2021-01-30 15:30:56 -0300

    Imported on windows; Improved player movement

--- LightAndFight/Assets/Scripts/PlayerMovementHandler.cs
+++ LightAndFight/Assets/Scripts/PlayerMovementHandler.cs
@@ -2,17 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementHandler : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlayerMovementHandler : MonoBehaviour {
+    public float speed = 10;
+    public float jumpForce = 10;
+
+    Rigidbody2D rb;
+
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        int horizontal = ReadHorizontal();
+        MoveHorizontal(horizontal);
+        ReadJump();
+    }
+
+    private void MoveHorizontal(int direction) {
+        transform.position += Vector3.right * direction * speed * Time.deltaTime;
+    }
+
+    private void ReadJump() {
+        if (Input.GetKeyDown(jump)) {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    private int ReadHorizontal() {
+        int horizontal = 0;
+        if (Input.GetKey(left)) {
+            horizontal = -1;
+        } else if (Input.GetKey(right)) {
+            horizontal = 1;
+        }
+        return horizontal;
     }
 }
