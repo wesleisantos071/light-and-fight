3866e95930caca7b60d79d9ce482eb6c04c7b6f4
jessicagoss <jessicamgoss@gmail.com>
2021-01-30 14:50:24 -0300

    Implement two player handling

---
+++ LightAndFight/Assets/Scripts/Player2MovementController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2MovementController : MonoBehaviour {
+
+    public float speed = 10;
+    public float jumpForce = 10;
+
+    Rigidbody2D rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        float horizontal = 0;
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            horizontal = -1;
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            horizontal = 1;
+        }
+        transform.position += Vector3.right * horizontal * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+}
