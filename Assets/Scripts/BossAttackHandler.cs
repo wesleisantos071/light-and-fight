36a7116e7d97e03efd483391e2ca64dc284696ba
jessicagoss <jessicamgoss@gmail.com>
2021-01-31 17:05:52 -0300

    FinishedSpit animation

---
+++ Assets/Scripts/BossAttackHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BossAttackHandler : MonoBehaviour {
+    public GameObject spitPrefab;
+    public GameObject playerTarget;
+    public float initialSpitTime = 4;
+    float currentSpitTime;
+    public float limitSpitTime = 0.8f;
+    public Action onSpit;
+
+    void Start() {
+        spitPrefab.GetComponent<BossSpitHandler>().playerLight = playerTarget;
+        currentSpitTime = initialSpitTime;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        currentSpitTime -= Time.deltaTime;
+        if (currentSpitTime <= 0) {
+            Spit();
+            currentSpitTime = initialSpitTime;
+        }
+    }
+
+    void Spit() {
+        GameObject spit = GameObject.Instantiate(spitPrefab);
+        spit.transform.position = transform.position;
+        spit.transform.parent = null;
+        onSpit?.Invoke();
+    }
+}
---
+++ Assets/Scripts/BossSpitHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpitHandler : MonoBehaviour {
+    public GameObject playerLight;
+    public float speed = 1;
+
+    // Update is called once per frame
+    void Update() {
+        if (playerLight != null) {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, playerLight.transform.position, step);
+            Rotate();
+        }
+    }
+
+    void Rotate() {
+        float rotationSpeed = 10f;
+        float offset = -160f;
+        Vector3 direction = playerLight.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle + offset, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag.Contains("Player")) {
+            Destroy(gameObject);
+        }
+    }
+}
